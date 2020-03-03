6c33713d4602282663bc289e5da4b11bc33e91ab
William McCray <william.mccray1@outloock.com>
2020-03-03 13:02:09 -0500

    Branching application for my portfolio version with increased functionality, polished layout, more efficient design

---
+++ Homework/cssbs-hw16.md
@@ -0,0 +1,23 @@
+# C# Homework 16
+
+#### --AJ McCray
+
+1. bits = ~11001100, bits |= (1 << 5), (bits & (1 << 5)) != 0, bits = (11001100 >> 2), bits &= ~(1 << 5)
+
+2. yes, bits = (11001100 >> 2)
+
+3. evaluates an and operation on 1 left shifted index number of times and bits
+
+4. set the index position to 1
+
+5. does an and operation on the index position using 1
+
+6. if bit position n is one set bool true if zero set bool false
+
+7. sets bit position n to 1
+
+8. applies an explicit or operation on bit position bit position n
+
+9. a) no permissions; b) read privilege's; c) write permissions; d) read and write; e) execute permissions; f) execute and read; g) execute and write; f) all permissions
+
+10. each permission is turned on or off via a bit position. If that bit is on the user has that permission otherwise they do not
---
+++ Quizes/CSSBS-Quiz08/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CSSBS_Quiz08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Firearm Pistol = new Firearm("Pistol", "Pop", "9mm");
+            Console.WriteLine(Pistol.ToString());
+            Firearm Rifle = new Firearm("Rifle", "bang", "5.56");
+            Console.WriteLine(Rifle.ToString());
+            Firearm Shotgun = new Firearm("Shotgun", "Pop", "12 gauge");
+            Console.WriteLine(Shotgun.ToString());
+        }
+    }
+    internal class Firearm
+    {
+        private static int Count { get; set; } = 0;
+        string Name { get; set; }
+        string Sound { get; set; }
+        string Caliber { get; set; }
+        public Firearm(string Name, string Sound, string Caliber)
+        {
+            this.Name = Name;
+            this.Sound = Sound;
+            this.Caliber = Caliber;
+            Count++;
+        }
+        public override string ToString()
+        {
+            return $"I am a {Name} and I go {Sound} with a {Caliber} round, and we have {Count} Firearms";
+        }
+    }
+}
