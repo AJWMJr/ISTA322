6894a4c7e6f6fe0996d06801f49b15c18cd9cad8
William McCray <william.mccray1@outloock.com>
2020-03-06 11:26:49 -0500

    adding roulette experimental approach

---
+++ Labs/MVC-Lab04/Models/ShoppingCarts.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MVC_Lab04.Models
+{
+    public class ShoppingCarts : IEnumerable<Product>
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Labs/MVC-Lab04/Startup.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MVC_Lab04
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+
+            /*
+            app.Run(async (content) => {
+                await context.Response.WriteAsync("Hello World");
+            });
+            */
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
